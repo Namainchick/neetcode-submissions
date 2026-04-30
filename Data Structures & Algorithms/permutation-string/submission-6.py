8af26535f76025183209ce791b1b94428b72e2d2
Namanh Bui Vu <nbui111111@gmail.com>
2026-04-30 12:20:17 +0200

    Add: permutation-string - submission-6

---
+++ Data Structures & Algorithms/permutation-string/submission-6.py
@@ -0,0 +1,8 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        foo = set(s1)
+        for i in range(len(s2)-len(s1)):
+            print(set(s2[i:i+len(s1)]),foo)
+            if foo == set(s2[i:i+len(foo)]):
+                return True
+        return False
