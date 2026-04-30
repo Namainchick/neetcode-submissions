15fa271ac23ba6172e37eef8c130fb13c7587fcd
Namanh Bui Vu <nbui111111@gmail.com>
2026-04-30 12:14:49 +0200

    Add: permutation-string - submission-1

---
+++ Data Structures & Algorithms/permutation-string/submission-1.py
@@ -0,0 +1,7 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        foo = tuple(s1)
+        for i in range(len(s2)-3):
+            if foo == tuple(s2[i:i+len(foo)]):
+                return True
+        return False
