a20dad65b400c9c1fafd4f049de7be3dcfcbdba6
Namanh Bui Vu <nbui111111@gmail.com>
2026-05-13 19:11:35 +0200

    Add: time-based-key-value-store - submission-0

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-0.py
@@ -0,0 +1,26 @@
+class TimeMap:
+
+    def __init__(self):
+        self.hashmap = defaultdict(list)
+        
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        if key not in self.hashmap:
+            self.hashmap[key].append((value,timestamp))
+        else:
+            for i in range(len(self.hashmap[key])):
+                _,time = self.hashmap[key][i]
+                if timestamp <= time:
+                    self.hashmap[key].insert(i,(value,timestamp))
+                    return 
+        self.hashmap[key].append((value,timestamp))
+
+
+    def get(self, key: str, timestamp: int) -> str:
+        if key not in self.hashmap:
+            return ""
+        else:
+            for i in range(len(self.hashmap[key])-1,-1,-1):
+                val,time = self.hashmap[key][i]
+                if timestamp >= time:
+                    return val
