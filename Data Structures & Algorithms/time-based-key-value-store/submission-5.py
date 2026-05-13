773483b111eb3e02901cf4067863546197a876d8
Namanh Bui Vu <nbui111111@gmail.com>
2026-05-13 19:34:39 +0200

    Add: time-based-key-value-store - submission-5

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-5.py
@@ -0,0 +1,29 @@
+class TimeMap:
+
+    def __init__(self):
+        self.hashmap = defaultdict(list)
+        
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        self.hashmap[key].append((value,timestamp))
+
+
+    def get(self, key: str, timestamp: int) -> str:
+        if key not in self.hashmap:
+            return ""
+        else:
+            l = 0
+            r = len(self.hashmap[key]) - 1
+            res = ""
+            while l <= r:
+                mid = (l+r) // 2
+                val,time = self.hashmap[key][mid]
+                if timestamp >= time:
+                    res = val
+                    l = mid + 1
+                else:
+                    r = mid - 1
+            return res
+
+# got it now with binary search for find leftest problem 
+
