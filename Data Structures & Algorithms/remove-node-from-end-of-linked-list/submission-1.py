b7fed4dd2d69fa70152b0b7a268a80f81b49b50e
Namanh Bui Vu <nbui111111@gmail.com>
2026-05-15 14:41:42 +0200

    Add: remove-node-from-end-of-linked-list - submission-1

---
+++ Data Structures & Algorithms/remove-node-from-end-of-linked-list/submission-1.py
@@ -0,0 +1,36 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+#
+# first approach would be brute forece storing all nodes    
+# with their indeces in map for example. Then going to that 
+# node and delete it O(n). Cant get better than that i guess 
+# i see hashap isnt even needed jsut getting the length is all we need
+
+
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        length = 0
+        node = head
+        #calc length
+        while node:
+            length += 1
+            node = node.next
+        #get index in list
+    
+        dummy = ListNode(0, head)
+        node = dummy
+        
+        #go to node before
+        for i in range(length-n):
+            node = node.next 
+
+        node.next = node.next.next
+
+        return dummy.next
+
+# pretty hard figuring out how to deal with 
+# the edge cases like what if first or last 
+# is getting deleted
