22be89745786e3856635a48b27c49f5a630a6929
Namanh Bui Vu <nbui111111@gmail.com>
2026-05-18 00:49:24 +0200

    Add: copy-linked-list-with-random-pointer - submission-2

---
+++ Data Structures & Algorithms/copy-linked-list-with-random-pointer/submission-2.py
@@ -0,0 +1,40 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+"""
+"""
+brute forece:
+the problem is how we know in the copy list which pointer to point to for the 
+random pointer. Maybe safe node with their indeces in a hashmap. this will take n^2
+"""
+class Solution:
+    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        node = head
+        nodemap = {}
+        copy = Node(0)
+        copy_head = copy
+
+        while node:
+            temp = Node(node.val)
+            copy.next = temp
+            nodemap[node] = temp
+            copy = copy.next
+            node = node.next
+
+        node = head
+        copy = copy_head
+
+        for i in range(len(nodemap)):
+            if node.random is None:
+                pass
+            else:
+                copy.next.random = nodemap[node.random]
+            node = node.next
+            copy = copy.next
+
+        return copy_head.next
+
