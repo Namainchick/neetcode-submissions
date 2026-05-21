fc06ba7155aeddc71c367921d3c16b3b2e67b8de
Namanh Bui Vu <nbui111111@gmail.com>
2026-05-21 21:47:23 +0200

    Add: lru-cache - submission-3

---
+++ Data Structures & Algorithms/lru-cache/submission-3.py
@@ -0,0 +1,23 @@
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.hashmap = {}
+        self.cache = []
+        self.size = 0
+        self.capacity = capacity
+        
+
+    def get(self, key: int) -> int:
+        return self.hashmap.get(key,-1)
+
+    def put(self, key: int, value: int) -> None:
+        if self.size == self.capacity:
+            last_key = self.cache.pop(0)
+            temp = self.hashmap.pop(last_key,None)
+        else:
+            self.size += 1
+        self.hashmap[key] = value
+        self.cache.append(key)
+        
+
+
