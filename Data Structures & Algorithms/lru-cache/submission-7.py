212621ab4ccb94dcac503fe5ce097c6782f1cc87
Namanh Bui Vu <nbui111111@gmail.com>
2026-05-22 23:08:56 +0200

    Add: lru-cache - submission-7

---
+++ Data Structures & Algorithms/lru-cache/submission-7.py
@@ -0,0 +1,52 @@
+class Node:
+
+    def __init__(self, val, next, prev, key):
+        self.val = val
+        self.next = next
+        self.prev = prev
+        self.key = key
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.length = 0
+        self.hashmap = {}
+        self.head = Node(0,None,None,0)
+        self.tail = self.head
+
+    def get(self, key: int) -> int:
+        if key in self.hashmap:
+            node = self.hashmap[key]
+            if node.next:
+                node.prev.next = node.next
+                node.next.prev = node.prev
+                node.next = None
+                node.prev = self.tail
+                self.tail.next = node
+                self.tail = self.tail.next
+            return node.val
+        else:
+            return -1
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.hashmap:
+            node = self.hashamp[key]
+            node.prev.next = node.next
+            node.next.prev = node.prev
+            node.next = None
+            node.prev = self.tail
+            self.tail.next = node
+
+        if self.length == self.capacity:
+            node = self.head.next
+            temp = self.head.next.next
+            self.head.next = temp
+            temp.prev = self.head
+            del self.hashmap[node.key]
+        else:
+            self.length += 1
+
+        self.tail.next = Node(value, None, self.tail, key)
+        self.hashmap[key] = self.tail.next
+        self.tail = self.tail.next
