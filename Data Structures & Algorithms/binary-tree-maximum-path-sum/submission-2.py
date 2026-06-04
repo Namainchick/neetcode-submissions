70b11218ee9990d2a4d69621f5929fe6c0c611e1
Namanh Bui Vu <nbui111111@gmail.com>
2026-06-04 23:29:58 +0200

    Add: binary-tree-maximum-path-sum - submission-2

---
+++ Data Structures & Algorithms/binary-tree-maximum-path-sum/submission-2.py
@@ -0,0 +1,44 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+"""
+so this kina looks like dp where you store the best possible but this doesnt work for paths. 
+I remember a graph algotigh doing something similar to this but im not sure if this is the right this to do here.
+Some thing like bfs but you have to color the nodes. 
+Another Idea is just traverse and set the max sum for the node but also tell the neighbours. problem is with trees its hard to go back because its 
+like a directed graph. 
+The final idea is: a node gets its parent best then we check ok for the node do we store right path or de we store left path or do we store path left to 
+right. If parent gives you <0 as best just skip the parent.
+"""
+
+class Solution:
+    def maxPathSum(self, root: Optional[TreeNode]) -> int:
+
+        self.best = float('-inf')
+        def dfs(node, best):
+            left,right,mid = 0,0,0
+            if node.left:
+                left = max(best,best+node.left.val)
+            if node.right:
+                right = max(best,best + node.right.val)
+            if node.right and node.left:
+                mid = max(best,node.val + node.left.val + node.right.val)
+            if not node.right and not node.left:
+                best = node.val
+            else:
+                best = max(left,right,mid)
+            self.best = max(self.best,best)
+            if node.left:
+                dfs(node.left,best)
+            if node.right:
+                dfs(node.right,best)
+
+        dfs(root, root.val)
+
+        return self.best
+            
+
