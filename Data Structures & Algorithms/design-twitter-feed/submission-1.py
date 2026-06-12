52f6e8085301c388e2f54590e6c3df10155d65c7
Namanh Bui Vu <nbui111111@gmail.com>
2026-06-12 23:36:46 +0200

    Add: design-twitter-feed - submission-1

---
+++ Data Structures & Algorithms/design-twitter-feed/submission-1.py
@@ -0,0 +1,31 @@
+"""
+very high level we need to store:
+- user following (should be fast readable and editable)
+- posts with priority. in this case time. (heap)
+"""
+
+class Twitter:
+
+    def __init__(self):
+        self.follows = defaultdict(list)
+        self.posts = []
+        self.time = 1
+
+    def postTweet(self, userId: int, tweetId: int) -> None:
+        heapq.heappush(self.posts,(-self.time,userId,tweetId))
+        self.time += 1
+
+    def getNewsFeed(self, userId: int) -> List[int]:
+        temp = self.posts.copy()
+        out = []
+        while temp and len(out) < 10:
+            _,uid,tid = heapq.heappop(temp)
+            if uid in self.follows[userId] or uid == userId:
+                out.append(tid)
+        return out 
+
+    def follow(self, followerId: int, followeeId: int) -> None:
+        self.follows[followerId].append(followeeId)
+
+    def unfollow(self, followerId: int, followeeId: int) -> None:
+        self.follows[followerId].remove(followeeId)
