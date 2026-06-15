0e039bca4922a430b49a60b12a3a118ce56e96cf
Namanh Bui Vu <nbui111111@gmail.com>
2026-06-16 00:34:46 +0200

    Add: subsets - submission-1

---
+++ Data Structures & Algorithms/subsets/submission-1.py
@@ -0,0 +1,20 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        result = []
+        temp = []
+        def backtrack(index: int):
+            if index >= len(nums):
+                result.append(temp.copy())
+                return 
+
+            temp.append(nums[index])
+            backtrack(index + 1)
+
+            temp.pop()
+            backtrack(index + 1)
+        backtrack(0)
+        return result
+
+"""
+how the fuck I cant think anymore its way too late
+"""
