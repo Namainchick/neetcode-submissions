07cf0313efcc762d888bea8277df2ff210848849
Namanh Bui Vu <nbui111111@gmail.com>
2026-06-17 16:32:45 +0200

    Add: permutations - submission-1

---
+++ Data Structures & Algorithms/permutations/submission-1.py
@@ -0,0 +1,16 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        result = []
+        def backtrack(temp, remain):
+            if not remain:
+                result.append(temp.copy())
+            for i in range(len(remain)):
+                temp.append(remain[i])
+                backtrack(temp, remain[:i] + remain[i+1:])
+                temp.pop()
+        backtrack([], nums.copy())
+        return result
+
+"""
+I kinda understand but also not really
+"""
