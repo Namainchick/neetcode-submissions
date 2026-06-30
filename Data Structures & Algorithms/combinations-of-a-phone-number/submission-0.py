24f69267483db8dc2638956c44c576e29706e40b
Namanh Bui Vu <nbui111111@gmail.com>
2026-06-30 21:42:31 +0200

    Add: combinations-of-a-phone-number - submission-0

---
+++ Data Structures & Algorithms/combinations-of-a-phone-number/submission-0.py
@@ -0,0 +1,13 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        hashmap = {
+            "2": "abc",
+            "2": "abc",
+            "2": "abc",
+            "2": "abc",
+            "2": "abc",
+            "2": "abc",
+            "2": "abc",
+            "2": "abc",
+            "2": "abc",
+        }
