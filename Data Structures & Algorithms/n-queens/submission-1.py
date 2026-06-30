fe3669338861f9bd07ae15ab97cd766f0240fe6d
Namanh Bui Vu <nbui111111@gmail.com>
2026-06-30 22:43:21 +0200

    Add: n-queens - submission-1

---
+++ Data Structures & Algorithms/n-queens/submission-1.py
@@ -0,0 +1,40 @@
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        column = set()
+        diag1 = set()  
+        diag2 = set()
+        
+        self.result = []
+
+        def backtrack(row,current):
+            if len(current) == n:
+                board = []
+                for col in current:
+                    board.append("." * col + "Q" + "." * (n - col - 1))
+                self.result.append(board.copy())
+                return 
+
+            for col in range(n):
+                if col not in column and (row-col) not in diag1 and (row+col) not in diag2:
+                    column.add(col)
+                    diag1.add(row-col)
+                    diag2.add(row+col)
+
+                    current.append(col)
+                    backtrack(row+1,current)
+                    current.pop()
+
+                    column.remove(col)
+                    diag1.remove(row-col)
+                    diag2.remove(row+col)
+
+       
+        backtrack(0,[])
+
+        return self.result
+
+
+"""
+easier than i thought: still pretty hard tho.
+"""
+        
