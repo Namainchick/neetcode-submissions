6e287f455a25fae1c955380b8aa10512503a2a51
Namanh Bui Vu <nbui111111@gmail.com>
2026-07-07 18:34:28 +0200

    Add: surrounded-regions - submission-3

---
+++ Data Structures & Algorithms/surrounded-regions/submission-3.py
@@ -0,0 +1,29 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        islands = []
+        safe = set()
+        directions = [(0,1),(0,-1),(1,0),(-1,0)]
+
+        ROW = len(board)
+        COL = len(board[0])
+
+        def dfs(i,j):
+            if (i,j) in safe:
+                return 
+            safe.add((i,j))
+            for x,y in directions:
+                ni,nj = i+x,j+y
+                if 0<=ni<ROW and 0<=nj<COL and (ni,nj) not in safe and board[ni][nj] == "O":
+                    dfs(ni,nj)
+
+        for i in range(ROW):
+            for j in range(COL):
+                if (i == 0 or j == 0 or i == ROW-1 or j == COL-1) and board[i][j] == "O":
+                    dfs(i,j)
+
+        for i in range(ROW):
+            for j in range(COL):
+                if board[i][j] == "O" and (i,j) not in safe:
+                    board[i][j] = "X"
+
+        
